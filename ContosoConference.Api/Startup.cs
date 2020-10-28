eb03cc199f06a1dcb298a09457fa7c9398d70e9c
Felipe Romero <feliperomero3@outlook.com>
2020-10-27 20:30:35 -0600

    Remove boilerplate comments.

--- ContosoConference.Api/Startup.cs
+++ ContosoConference.Api/Startup.cs
@@ -18,7 +18,6 @@ namespace ContosoConference.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMicrosoftIdentityWebApiAuthentication(Configuration);
@@ -31,7 +30,6 @@ namespace ContosoConference.Api
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
