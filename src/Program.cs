d7325f71ed4657913507a38bf4a9f298dcb15ae4
Felipe Romero <feliperomero3@outlook.com>
2025-03-01 18:39:29 -0600

    Migrate to the new minimal hosting model.

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using ContosoConference.Api.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 
 namespace ContosoConference.Api;
 
@@ -13,13 +16,80 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
-    }
+        var builder = WebApplication.CreateBuilder(args);
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
+        builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration);
+
+        builder.Services.AddControllers(options =>
+        {
+            var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+
+            options.Filters.Add(new AuthorizeFilter(policy));
+
+            var outputFormatter = options.OutputFormatters.OfType<SystemTextJsonOutputFormatter>().FirstOrDefault();
+
+            outputFormatter?.SupportedMediaTypes.Remove("text/json");
+        });
+
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
             {
-                webBuilder.UseStartup<Startup>();
+                Title = "Contoso Conference API",
+                Description = "A sample API with information related to a technical conference. " +
+                    "The available resources include *Speakers*, *Sessions* and *Topics*. " +
+                    " A single write operation is available to provide feedback on a Session.",
+                Version = "v1"
             });
+            c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.OAuth2,
+                Flows = new OpenApiOAuthFlows
+                {
+                    Implicit = new OpenApiOAuthFlow
+                    {
+                        AuthorizationUrl = new Uri(builder.Configuration["OpenApi:AuthorizationUrl"]!),
+                        Scopes = new Dictionary<string, string>
+                        {
+                            { builder.Configuration["OpenApi:Scope"]!, builder.Configuration["OpenApi:ScopeDescription"] }
+                        }
+                    }
+                }
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+                    },
+                    new[] { builder.Configuration["OpenApi:Scope"] }
+                }
+            });
+            c.OperationFilter<ContentTypeOperationFilter>();
+            c.OperationFilter<AuthResponsesOperationFilter>();
+        });
+
+        var app = builder.Build();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.UseSwagger();
+
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint(builder.Configuration["OpenApi:SwaggerEndpointUrl"], builder.Configuration["OpenApi:SwaggerEndpointName"]);
+            c.OAuthClientId(builder.Configuration["OpenApi:ClientId"]);
+            c.OAuthRealm(builder.Configuration["OpenApi:Realm"]);
+            c.OAuthAppName(builder.Configuration["OpenApi:AppName"]);
+        });
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
